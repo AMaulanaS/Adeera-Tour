3fb13f0062101bcc9ad2bd072f06e590b705108e
Aku Alan Favoritmu <aditymaulino@gmail.com>
2022-10-12 13:02:14 +0700

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,51 @@
+
+
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+          
+       
+            // PRETTYPHOTO FUNCTION 
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+          
+            /*====================================
+               WRITE YOUR SCRIPTS BELOW 
+           ======================================*/
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
